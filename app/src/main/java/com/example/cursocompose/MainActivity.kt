6fc08b3657ca3eef837c8fdb610a171d3c239176
George <mr.georgemanea@gmail.com>
2025-12-07 17:44:46 +0100

    pt2

--- app/src/main/java/com/example/cursocompose/MainActivity.kt
+++ app/src/main/java/com/example/cursocompose/MainActivity.kt
@@ -1,25 +1,29 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.cursocompose
 
 import androidx.compose.ui.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -27,26 +31,63 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cursocompose.ui.theme.CursoComposeTheme
-import androidx.compose.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            app()
+            ViewContainer()
         }
     }
 }
 
+@Composable
+fun FAB(){
+    val context = LocalContext.current
+    FloatingActionButton(onClick = { Toast.makeText(context, "Toast", Toast.LENGTH_SHORT).show()
+    }){
+        Text("X")
+    }
+}
+
+
+@Composable
+fun ViewContainer() {
+    val context = LocalContext.current
+
+    Scaffold(
+        topBar = { Toolbar() },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                Toast.makeText(context, "Toast", Toast.LENGTH_SHORT).show()
+            }) {
+                Text("X")
+            }
+        }
+    ) { padding ->
+        Contenido(modifier = Modifier.padding(padding))
+    }
+}
+
+
+@Composable
+fun Toolbar(){
+    TopAppBar(title = {Text(text = "Sriracha App")}, Modifier.background(Color.Cyan))
+}
+
+
+
+
 @Preview
 @Composable
-fun app(){
-    var counter by rememberSaveable{ mutableStateOf(0) }
+fun Contenido(modifier: Modifier = Modifier) {
+    var counter by rememberSaveable { mutableStateOf(0) }
 
-    LazyColumn (modifier = Modifier.fillMaxSize()) {
+    LazyColumn (modifier = Modifier.fillMaxSize().background(Color.LightGray).padding(top = 100.dp)) {
         item{
         Image(painter = painterResource(id = R.drawable.unnamed),
                 contentDescription = null,
@@ -93,22 +134,5 @@ fun app(){
 
             )
         }
-    }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CursoComposeTheme {
-        Greeting("Android")
     }
 }
